3e0e38458086cbee70eb4c3796a9502076658c6e
RA-Gagan <gagan.a@byjus.com>
2022-12-13 22:22:19 +0530

    feat: :fire: changed todo list design\

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import './App.css';
+import "./App.css";
+import { Helmet } from "react-helmet";
 
-import Todopage from './Components/todopage';
+import Todopage from "./Components/todopage";
+import { Fragment } from "react";
 
 function App() {
   return (
-
-    <Todopage/>
-  )
-    
- 
+    <Fragment>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>To-Do List</title>
+      </Helmet>
+      <Todopage />
+    </Fragment>
+  );
 }
 
 export default App;
--- src/Components/todopage.js
+++ src/Components/todopage.js
@@ -1,10 +1,14 @@
 import React, { useRef, useState } from "react";
-import { Button } from "@mui/material";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SendIcon from "@mui/icons-material/Send";
+import Stack from "@mui/material/Stack";
+import { Fragment } from "react";
 
 const Todopage = () => {
   const [tastlist, seTtaskList] = useState([]);
   const ref = useRef("");
-  
+
   const addTask = () => {
     seTtaskList((old) => {
       return [...old, ref.current.value];
@@ -13,31 +17,51 @@ const Todopage = () => {
   };
 
   return (
-    <div className=" flex h-screen justify-center items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ...">
-      <div className="box-border bg-slate-300 p-4 rounded-lg ... shadow-2xl">
-        <div className="flex justify-center my-8 ">
-          <h1 className="text-2xl text-purple-500 underline decoration-wavy ... ">
-            To-do List
-          </h1>
-        </div>
-        <input
-          type="text"
-          className=" border-4 to-blue-600 rounded-full ... focus:border-blue-400 ... mx-2"
-          ref={ref}
-        />
-        <Button onClick={addTask} className="mx-2">
-          ADD
-        </Button>
-        <Button onClick={() => seTtaskList([])} className="mx-2">
-          clear
-        </Button>
-        <div>
-          {tastlist.map((todo, index) => (
-            <li>{todo}</li>
-          ))}
+    <Fragment>
+      <div className="h-screen bg-gradient-to-r from-violet-500 to-fuchsia-500">
+        <div className="flex h-screen justify-center items-center">
+          <div className=" box-border bg-yellow-100 w-50 p-4 shadow-2xl ...  rounded-xl">
+            <div className="flex justify-center m-5 box-border bg-gradient-to-r from-purple-500 to-pink-500 p-4 rounded-lg ... shadow-2xl">
+              <h1 className="text-2xl font-Michroma text-black underline">
+                To-do List
+              </h1>
+            </div>
+
+            <div className="flex flex-wrap justify-between m-4">
+              <input
+                type="text"
+                className="border-4 to-blue-600 rounded-full ... focus:border-blue-400 ... mx-2"
+                ref={ref}
+              />
+              <Stack direction="row" spacing={2} className="m-2">
+                <Button
+                  type="button"
+                  color="success"
+                  variant="contained"
+                  endIcon={<SendIcon />}
+                  onClick={addTask}
+                >
+                  ADD
+                </Button>
+                <Button
+                  variant="contained"
+                  color="error"
+                  startIcon={<DeleteIcon />}
+                  onClick={() => seTtaskList([])}
+                >
+                  clear
+                </Button>
+              </Stack>
+            </div>
+            <div className="font-Kalam box-border   text-2xl">
+              {tastlist.map((todo, index) => (
+                <li>{todo}</li>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
 
